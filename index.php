f316fde769acdce3601b1cf2a492243d58cc60a8
sofasurfer <kilian@sofasurfer.ch>
2020-06-30 14:00:57 +0200

    Add streaming links

--- index.php
+++ index.php
@@ -69,14 +69,14 @@
 
             $url = 'http://tourdate.twogentlemen.net/json.php?id=413';
             $content = file_get_contents($url);
-            $jsonExternal = json_decode($content, true);
+            $json = json_decode($content, true);
 
-            $url = __DIR__ . "/assets/data/shows.json";
-            $content = file_get_contents($url);
-            $jsonLocal = json_decode($content, true);
+            // $url = __DIR__ . "/assets/data/shows.json";
+            // $content = file_get_contents($url);
+            // $jsonLocal = json_decode($content, true);
 
-            $json = array();
-            $json['shows'] = array_merge($jsonExternal['shows'],$jsonLocal['shows']);
+            // $json = array();
+            // $json['shows'] = array_merge($jsonExternal['shows'],$jsonLocal['shows']);
             usort($json['shows'], function($a, $b) {
                 return $a['show_date'] <=> $b['show_date'];
             });
@@ -163,6 +163,41 @@
           <center><p style="margin-top: 50px;"><a title="Shop only for france" class="buy france" target="_blank" href=" http://www.wiseband.com/yo/shop.php?boutique=5501">Online shop france</a></p></center>
         </div>
       </div>
+
+
+        <div class="row stream-list  bounce-up">
+            <h2>Stream</h2>
+            <div class="col-md-4 col-xs-6 bounce-up animation-element">
+              <div class="subject">
+                <a target="_blank" href="https://music.apple.com/de/artist/puts-marie/217937086">
+                  <img class="img-responsive" src="/assets/images/n_apple_music.png">
+                </a>
+              </div>
+            </div>
+            <div class="col-md-4 col-xs-6 bounce-up animation-element">
+              <div class="subject">
+                <a target="_blank" href="https://open.spotify.com/artist/14h4AzYpNeMJh7HhHDx6gV">
+                  <img class="img-responsive" src="/assets/images/n_spotify.png">
+                </a>
+              </div>
+            </div>
+            <div class="col-md-4 col-xs-6 bounce-up animation-element">
+              <div class="subject">
+                <a target="_blank" href="https://www.deezer.com/de/artist/868984">
+                  <img class="img-responsive" src="/assets/images/n_deezer.png">
+                </a>
+              </div>
+            </div>   
+        </div>
+        <!--div class="row stream-list">
+            <div class="col-md-12">
+                <h2>Buy</h2>
+            </div>
+            <div class="col-md-4 col-xs-6"><a target="_blank" href="https://music.apple.com/ch/album/jo-sicher/1500271075?app=itunes&ign-mpt=uo%3D4"><img class="img-responsive" src="/assets/images/n_itunes.png"></a></div>
+            <div class="col-md-4 col-xs-6"><a target="_blank" href="https://play.google.com/store/music/album/Studeyeah_Jo_Sicher?id=Bprgxe6damfc4oai7x5sz4hp4du"><img class="img-responsive" src="/assets/images/n_google_play.png"></a></div>
+            <div class="col-md-4 col-xs-6"><a target="_blank" href="https://www.amazon.de/dp/B08546F5X4?tag=igroove0a-21"><img class="img-responsive" src="/assets/images/n_amazon.png"></a></div>    
+        </div-->
+
     </div>
     <?php include 'tpl/footer.php'; ?>
 
