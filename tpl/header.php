455cdf3306e637ed738d4bde55545d398c1c3642
sofasurfer <kilian@sofasurfer.ch>
2022-07-06 16:18:27 +0200

    Change meta description

--- tpl/header.php
+++ tpl/header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">  
     <title>Puts Marie | Official Website</title>
-    <meta name="description" content="Nick Porsche - drums, Igor Stepniewski - bass, Sirup Gagavil - guitar, Max Usata - vocal, Beni 06 - Farfisa">
+    <meta name="description" content="Tobi Schramm - drums, Igor Stepniewski - bass, Sirup Gagavil - guitar, Max Usata - vocal, Beni 06 - Farfisa">
     <meta name="keywords" content="pop, punk, indie, hip-hop, chanson, rock, folk, swing, rap, surf, yagwud">
     <meta name="google-site-verification" content="2KC6nTlKXS1xgG15jEm0F67bnvosRzSYjOXX8G4eCGM" />
 
